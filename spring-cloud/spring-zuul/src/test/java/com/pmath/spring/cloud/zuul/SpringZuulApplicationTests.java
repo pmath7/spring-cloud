5a63d81c944ec296342f208aa67cb294dafb0f14
pmath7 <kmucsc@yeah.net>
2020-02-03 10:04:11 +0800

    spring-cloud demo,涉及eureka,ribbon,feign,hystrix,zuul,config最简单的配置

---
+++ spring-cloud/spring-zuul/src/test/java/com/pmath/spring/cloud/zuul/SpringZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmath.spring.cloud.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
